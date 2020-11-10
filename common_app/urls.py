323c70e404d9ae1ea2c6da5e9e8e9fe5d37252b3
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-11-10 21:38:32 +0200

    html login

--- common_app/urls.py
+++ common_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from common_app import views
 
 urlpatterns = [
-    path('', views.articles_list, name='articles')
+    path('', views.homepage, name='home')
 
 ]
--- common_app/views.py
+++ common_app/views.py
@@ -5,40 +5,21 @@ from django.shortcuts import render
 from common_app.models import Article
 
 
-def articles_list(request):
-    params = {
-        'token': 'sgfnw2wtulfkxlbid2dg',
-        'currency': 'EUR/USD USD/JPY GBP/USD USD/CHF AUD/USD NZD/USD USD/CAD'
-    }
-    api_result = requests.get('https://currencydatafeed.com/api/data.php', params)
-
-    api_response = api_result.json()
-    currencies=api_response['currency']
-    print(currencies)
+def homepage(request):
+    # params = {
+    #     'token': 'sgfnw2wtulfkxlbid2dg',
+    #     'currency': 'EUR/USD USD/JPY GBP/USD AUD/USD USD/CAD'
+    # }
+    # api_result = requests.get('https://currencydatafeed.com/api/data.php', params)
+    #
+    # api_response = api_result.json()
+    # currencies = api_response['currency']
 
     context = {
         'articles': Article.objects.all(),
-        'currencies': currencies
+        # 'currencies': currencies
 
     }
 
-    # print(api_response)
 
-    # params = {
-    #     'apikey': 'ZX2ZPRW3YWJ9I49Z',}
-    # base_url = r"https://www.alphavantage.co/query?function = CURRENCY_EXCHANGE_RATE"
-    # main_url = base_url
-    # req_ob = requests.get(base_url,params)
-    # result = req_ob.json()
-    # print(result)
-
-    # params = {
-    #     'access_key': '6debc7e077ca0ad17f00166e0093e432',
-    #
-    # }
-    #
-    # api_result = requests.get('http://api.marketstack.com/v1/currencies', params)
-    #
-    # api_response = api_result.json()
-    # print(api_response)
     return render(request, 'common_app/index.html', context)
